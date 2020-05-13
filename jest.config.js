be494e310bb39d632f8dd273f5dc1a51b6a5f128
Kai <tehhavok@gmail.com>
2020-05-13 17:50:58 -0400

    Add support for image imports in tested components

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,8 @@ module.exports = {
   moduleFileExtensions: ["js", "json", "jsx", "node"],
   testPathIgnorePatterns: ["/node_modules/"],
   moduleNameMapper: {
-    "\\.(css|scss)$": require.resolve("./test/style-mock")
+    "\\.(css|scss)$": require.resolve("./test/style-mock"),
+    "\\.(gif|jpg|png|svg)$": require.resolve("./test/file-mock.js")
   },
   setupTestFrameworkScriptFile: require.resolve("./test/setup-test-framework"),
   verbose: true
